7d959ee86f4b73f66a3b0c478cc0e8eaaad39231
wisarut <wisarut.sri@eoss-th.com>
2015-09-01 22:19:22 +0700

    Initial Commit

---
+++ PKem IQ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PKem IQ
+//
+//  Created by EOSS-TH on 9/1/2558 BE.
+//  Copyright (c) 2558 EOSS-TH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
